e90c9b6363394ffa6fbba4544724adf87a4871ef
Mochammad Rizky Diprasetya <software@rizkydiprasetya.com>
2023-10-15 09:23:50 +0200

    change sha256

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -2,7 +2,7 @@ class Sshpass < Formula
   desc "Non-interactive ssh password auth Files"
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
-  sha256 "39f09dd59811256357fbe83d25541d15e995802b40d284dfb30eddd4262d60a8"
+  sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
   license "GPL-2.0-or-later"
 
   def install
